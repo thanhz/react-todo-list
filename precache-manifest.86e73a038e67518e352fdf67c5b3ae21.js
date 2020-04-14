3a38bb4d765eeb2f24ba50320ef836a679d7a215
Thanh <thanhz@hotmail.com>
2020-04-15 00:50:06 +0100

    Updates

---
+++ precache-manifest.86e73a038e67518e352fdf67c5b3ae21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcf1abeb568438e786060317949ec6f",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "2788903c9894cd1aed1c",
+    "url": "/react-todo-list/static/css/main.975636c5.chunk.css"
+  },
+  {
+    "revision": "21059f7702cf8bfe4580",
+    "url": "/react-todo-list/static/js/2.994243f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.994243f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2788903c9894cd1aed1c",
+    "url": "/react-todo-list/static/js/main.fee7ce0b.chunk.js"
+  },
+  {
+    "revision": "6f50a3fd25b094239ce0",
+    "url": "/react-todo-list/static/js/runtime-main.ba336d73.js"
+  }
+]);
